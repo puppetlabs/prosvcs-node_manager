264dce3fbfaacd6f8523bb009bbef887cd00eae5
Jordan Olshevski <jordanolshevski@gmail.com>
2015-11-03 20:17:06 -0600

    (Issue #18) Retrieve classes for another env
    This commit allows users to optionally pass a single argument when
    calling environment_classes, which specifies the environment name from
    which to return the list of available classes.

--- lib/puppet/parser/functions/environment_classes.rb
+++ lib/puppet/parser/functions/environment_classes.rb
@@ -1,10 +1,19 @@
-Puppet::Parser::Functions.newfunction(:environment_classes, :arity => 0, :type => :rvalue) do |values|
+Puppet::Parser::Functions.newfunction(:environment_classes, :arity => -1, :type => :rvalue) do |values|
+  env_name = values[0]
+
+  if env_name
+    env = Puppet.lookup(:environments).get!(env_name)
+  else
+    # If no environment was provided, use this node's
+    env = environment
+  end
+
   # Make sure we've parsed and evaluated every class
   # available in the environment.
-  environment.check_for_reparse
-  environment.known_resource_types.loader.import_all
+  env.check_for_reparse
+  env.known_resource_types.loader.import_all
 
-  classes = environment.known_resource_types.hostclasses.map {|name, hostclass| hostclass.name }
+  classes = env.known_resource_types.hostclasses.map {|name, hostclass| hostclass.name }
   classes.delete("")
 
   return classes

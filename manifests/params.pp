e566f67c6a73ed548b2d2fc1e77f2ce1d04c340a
Martin Jackson <mhjacks@swbell.net>
2016-11-26 10:32:53 -0600

    Update for new PE version and new puppetclassify by default

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 class node_manager::params {
-  $version = '0.1.3' #puppetclassify gem version
+  $version = '0.1.5' #puppetclassify gem version
 
   if "$::puppetversion" =~ /3.8/ {
     $gemprovider='pe_gem'
   }
-  elsif "$::pe_server_version" =~ /2015/ {
+  elsif "$::pe_server_version" =~ /2015/ or "$::pe_server_version" =~ /2016/ {
     $gemprovider = 'puppet_gem'
   }
   else {
